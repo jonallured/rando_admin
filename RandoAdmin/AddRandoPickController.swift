d80e247f0c913ac5d2a109af58892ac50766fcb5
Jon Allured <jon.allured@gmail.com>
2017-09-13 22:41:45 -0500

    Wire up the Rando side of things

--- RandoAdmin/AddRandoPickController.swift
+++ RandoAdmin/AddRandoPickController.swift
@@ -1,36 +1,40 @@
 import UIKit
 
 class AddRandoPickController: UIViewController {
-  let randomPick = RandomPick()
+    let randomPick = RandomPick()
 
-  var rando: Rando! {
-    didSet {
-        randomPick.generate(rando: rando)
+    var rando: Rando! {
+        didSet {
+            randomPick.generate(rando: rando)
+        }
     }
-  }
 
-  @IBOutlet weak var teamNameLabel: UILabel!
-  @IBOutlet weak var saveButtonItem: UIBarButtonItem!
-  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var teamNameLabel: UILabel!
+    @IBOutlet weak var saveButtonItem: UIBarButtonItem!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
-  @IBAction func savePressed(sender: AnyObject) {
-    randomPick.save()
-  }
+    @IBAction func savePressed(sender: AnyObject) {
+        randomPick.save()
+    }
 
-  override func viewDidLoad() {
-    randomPick.delegate = self
-  }
+    override func viewDidLoad() {
+        randomPick.delegate = self
+    }
 }
 
 extension AddRandoPickController: RandomPickDelegate {
-  func didGenerate() {
-    activityIndicator.stopAnimating()
-    teamNameLabel.text = randomPick.teamName
-    teamNameLabel.isHidden = false
-    saveButtonItem.isEnabled = true
-  }
+    func didGenerate() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.teamNameLabel.text = self.randomPick.teamName
+            self.teamNameLabel.isHidden = false
+            self.saveButtonItem.isEnabled = true
+        }
+    }
 
-  func didSave() {
-    dismiss(animated: true, completion: nil)
-  }
+    func didSave() {
+        DispatchQueue.main.async {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
 }
--- RandoAdmin/RandomPick.swift
+++ RandoAdmin/RandomPick.swift
@@ -1,62 +1,57 @@
 import Foundation
 
 protocol RandomPickDelegate {
-  func didGenerate()
-  func didSave()
+    func didGenerate()
+    func didSave()
 }
 
 class RandomPick {
-  var delegate: RandomPickDelegate?
-  var rando: Rando?
-  var team: Team?
-//  var router: Router
+    var delegate: RandomPickDelegate?
+    var rando: Rando?
+    var team: Team?
 
-  var teamName: String {
-    return team?.name ?? ""
-  }
+    var teamName: String {
+        return team?.name ?? ""
+    }
 
-    init() { //(router: Router = ApiRouter.instance) {
-//    self.router = router
-  }
+    func generate(rando: Rando) {
+        self.rando = rando
+        let endpoint = Endpoint.activeTeams(weekNumber: rando.nextWeekNumber)
+        Router.hit(endpoint, handler: handleActiveTeamsResponse)
+    }
 
-  func generate(rando: Rando) {
-//    self.rando = rando
-//
-//    let params = ["week_number": String(rando.nextWeekNumber)]
-//    if let path = DynamicEndpoint(endpoint: .ActiveTeams, params: params).path {
-//      router.get(path, params: params, completion: handleActiveTeamsResponse)
-//    }
-  }
+    func save() {
+        guard let weekNumber = rando?.nextWeekNumber,
+            let teamId = team?.id
+            else { return }
 
-  func save() {
-//    guard let weekNumber = rando?.nextWeekNumber, let teamId = team?.id else { return }
-//    let params = ["week_number" : weekNumber, "team_id": teamId]
-//    router.post(.RandoPicks, params: params, completion: handleSaveRandoPickResponse)
-  }
+        let endpoint = Endpoint.createRandoPick(teamId: teamId, weekNumber: weekNumber)
+        Router.hit(endpoint, handler: handleSaveRandoPickResponse)
+    }
 }
 
 extension RandomPick {
-//  private func handleActiveTeamsResponse(response: Response) {
-//    guard response.isSuccess, let teamIds = response.json as? [Int] else { return }
-//
-//    let index = Int(arc4random_uniform(UInt32(teamIds.count)))
-//    let teamId = teamIds[index]
-//
-//    team = TeamStore.withId(id: teamId)
-//
-//    DispatchQueue.main.async {
-//      self.delegate?.didGenerate()
-//    }
-//  }
-//
-//  private func handleSaveRandoPickResponse(response: Response) {
-//    guard response.code == 201, let json = response.json else { return }
-//
-//    let attributes = PickAttributes.fromJSON(json: json)
-//    rando?.picks = attributes.map { Pick($0) }
-//
-//    DispatchQueue.main.async {
-//        self.delegate?.didSave()
-//    }
-//  }
+    private func handleActiveTeamsResponse(data: Data?, response: URLResponse?, error: Error?) {
+        guard let data = data,
+            let json = try? JSONSerialization.jsonObject(with: data, options: []),
+            let teamIds = json as? [Int]
+            else { return }
+
+        let index = Int(arc4random_uniform(UInt32(teamIds.count)))
+        let teamId = teamIds[index]
+        team = TeamStore.withId(id: teamId)
+
+        self.delegate?.didGenerate()
+    }
+
+    private func handleSaveRandoPickResponse(data: Data?, response: URLResponse?, error: Error?) {
+        guard let data = data,
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            else { return }
+
+        let attributes = PickAttributes.fromJSON(json: json)
+        rando?.picks = attributes.map { Pick($0) }
+
+        self.delegate?.didSave()
+    }
 }
